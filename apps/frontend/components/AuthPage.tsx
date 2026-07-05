209ccdf6960e8b587b6f44d6f77047d8936b8341
Nalin Dalal <nalindalal2004@gmail.com>
2026-07-05 12:13:34 +0530

    fix: AuthPage.tsx to call the API and RoomCanvas.tsx to use the stored token.

--- apps/frontend/components/AuthPage.tsx
+++ apps/frontend/components/AuthPage.tsx
@@ -1,16 +1,65 @@
 "use client";
 
+import { HTTP_BACKEND } from "@/config";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export function AuthPage({ isSignin }: { isSignin: boolean }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const router = useRouter();
+
+  async function handleClick() {
+    const res = await axios.post(
+      `${HTTP_BACKEND}/${isSignin ? "signin" : "signup"}`,
+      {
+        username: email,
+        password,
+        name,
+      },
+    );
+
+    if (isSignin) {
+      localStorage.setItem("token", res.data.token);
+      router.push("/canvas/1");
+    } else {
+      router.push("/signin");
+    }
+  }
+
   return (
     <div className="flex justify-center items-center w-screen h-screen">
       <div className="p-6 m-2 bg-white rounded">
         <div className="p-2">
-          <input type="text" placeholder="Email"></input>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-        <div className="p-2"></div>
-
+        {!isSignin && (
+          <div className="p-2">
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+        )}
+        <div className="p-2">
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
         <div className="pt-2">
-          <button className="p-2 bg-red-200 rounded" onClick={() => {}}>
+          <button className="p-2 bg-red-200 rounded" onClick={handleClick}>
             {isSignin ? "Sign in" : "Sign up"}
           </button>
         </div>
--- apps/frontend/components/RoomCanvas.tsx
+++ apps/frontend/components/RoomCanvas.tsx
@@ -1,26 +1,26 @@
 "use client";
 
 import { WS_URL } from "@/config";
-import { initDraw } from "@/draw";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Canvas } from "./Canvas";
 
 export function RoomCanvas({ roomId }: { roomId: string }) {
   const [socket, setSocket] = useState<WebSocket | null>(null);
 
   useEffect(() => {
-    const ws = new WebSocket(
-      `${WS_URL}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI4NGRlMzU1My0yNThmLTQ3YmMtODdlYy1iYTI5MWIwODg0YmEiLCJpYXQiOjE3MzcyMTczMjF9.b2Q3B-RUDAV0equhOjET7Hyl1AQaBf1FW4szk6mvGxU`,
-    );
+    const token = localStorage.getItem("token");
+    if (!token) return;
+
+    const ws = new WebSocket(`${WS_URL}?token=${token}`);
 
     ws.onopen = () => {
       setSocket(ws);
-      const data = JSON.stringify({
-        type: "join_room",
-        roomId,
-      });
-      console.log(data);
-      ws.send(data);
+      ws.send(
+        JSON.stringify({
+          type: "join_room",
+          roomId,
+        }),
+      );
     };
   }, []);
 
